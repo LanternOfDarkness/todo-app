7cbe3c8ab1453a22213345b2ac50ef6df270a3d3
Oleksii Velychko <kinlo443@gmail.com>
2025-07-30 14:54:49 +0300

    Made TaskItem component from task.

---
+++ src/Components/TaskItem.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function TaskItem(props: { title: string }) {
+    const [completed, setCompleted] = useState(false);
+
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <button onClick={() => setCompleted(!completed)}>
+                {completed ? "✅ Завершено" : "⬜ Активне"}
+            </button>
+        </div>
+    );
+}
