bac8753ef8bd8581f75e8e4edb1099a5ae71c051
Oleksii Velychko <kinlo443@gmail.com>
2025-07-30 15:50:30 +0300

    Complete task 3 of day 1.

--- src/Components/TaskList.tsx
+++ src/Components/TaskList.tsx
@@ -13,10 +13,25 @@ function TaskList() {
         { id: 2, title: "Task 2", completed: false },
     ]);
 
+    const [newTaskTitle, setTaskTitle] = useState<string>("");
+
+    const addTask = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!newTaskTitle.trim()) return;
+        
+        const id = Date.now();
+        setTasks([...tasks, { id, title: newTaskTitle, completed: false }]);
+        setTaskTitle("");
+    }
+
     return (
         <div>
+            <form onSubmit={(e)=> addTask(e)}>
+                <input type="text" value={newTaskTitle} onChange={(e) => setTaskTitle(e.target.value)} ></input>
+                <button type="submit">Add task</button>
+            </form>
             {tasks.map(task => (
-                <TaskItem key={task.id} title={task.title} completed={task.completed}/>
+                <TaskItem key={task.id} title={task.title} completed={task.completed} />
             ))}
         </div>
     );
